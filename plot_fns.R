f3ea9ff299f9bdaa18b425a72f83e55b2e881980
Claborne, Daniel <daniel.claborne@pnnl.gov>
2019-11-01 15:44:58 -0700

    edit plot fn

--- plot_fns.R
+++ plot_fns.R
@@ -2,3 +2,8 @@ price_v_time <- function(df){
   ggplot(df, aes(x = Date, y = mp_price, group = Date)) + geom_boxplot(outlier.size = 1) +
   labs(y = "Price", x = "Date")
 }
+
+mean_price_v_time <- function(df){
+  ggplot(df, aes(x = Date, y = mean_price)) + geom_line() +
+    labs(y = "Price", x = "Date")
+}
